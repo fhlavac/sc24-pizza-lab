efe0c8c8a57e7a79feba7a15dae0d1524e1404a8
Simon Soucek <simon.s.4627@gmail.com>
2024-08-06 07:36:06 -0400

    shdh

--- src/views/DrinkMenu.js
+++ src/views/DrinkMenu.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import { Box, Container, Typography } from '@mui/material';
-
+import { MyButton } from '../components/MyButton';
 const DrinkMenu = () => {
   return (
-    <Box sx={{ py: 4, background: '#F2F0EA' }}>
-      <Container maxWidth="lg" sx={{ height: '735px' }}>
-        <Typography variant="h3" gutterBottom>
-          Our Drink Menu
-        </Typography>
-      </Container>
-    </Box>
+    <>
+      <Box sx={{ py: 4, background: '#F2F0EA' }}>
+        <Container maxWidth="lg" sx={{ height: '735px' }}>
+          <Typography variant="h3" gutterBottom>
+            Our Drink Menu
+          </Typography>
+        </Container>
+      </Box>
+      <MyButton
+        sx={{
+          '&:hover': { color: 'blue' },
+        }}
+        variant="contained"
+      >
+        foo
+      </MyButton>
+    </>
   );
 };
 
